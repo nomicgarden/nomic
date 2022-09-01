19c241ea61161c9f7686dc88c8634ad6f8f257e6
Cosmo Guerini <cosmo.guerini@bostonfusion.com>
2022-09-01 00:00:13 -0400

    Add markets

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,6 @@
 <main>
     <h1>NOMIC</h1>
+    <a href="https://manifold.markets/cos/win-100-how-much-cash-will-nomic-ha-be4cb3906a73">[Win $100] How much cash will Nomic have in its treasury on December 31st, 2022?</a>
+    <a href="https://manifold.markets/cos/will-i-launch-my-digital-futarchy-b">Will I launch my digital futarchy before August 31st, 2022?</a>
+    <a href="https://manifold.markets/cos/how-many-unique-users-will-my-digit">How many unique users will my digital futarchy have on July 31st, 2023 if I name it "Nomic"?</a>
 </main>
