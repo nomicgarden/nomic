dc0cce7015f989c2c3d63c9b2959964b04375e39
Cosmo Guerini <cosmo.guerini@bostonfusion.com>
2022-08-31 23:39:30 -0400

    Add index and treasury pages

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,58 +1,26 @@
 <script>
-  import Header from '$lib/header/Header.svelte';
-  import { webVitals } from '$lib/vitals';
-  import { browser } from '$app/env';
-  import { page } from '$app/stores';
-  import '../app.css';
-
-  let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
-
-  $: if (browser && analyticsId) {
-    webVitals({
-      path: $page.url.pathname,
-      params: $page.params,
-      analyticsId
-    })
-  }
+  import "../app.css";
 </script>
 
-<Header />
+<svelte:head>
+  <meta name="robots" content="noindex" />
+</svelte:head>
 
-<main>
-  <slot />
-</main>
+<nav>
+  <a href=".">NOMIC</a>
+  <a href="/treasury">TREASURY</a>
+</nav>
 
-<footer>
-  <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
+<slot/>
 
 <style>
-  main {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
+  nav {
     padding: 1rem;
-    width: 100%;
-    max-width: 1024px;
-    margin: 0 auto;
-    box-sizing: border-box;
+    box-shadow: -1px 1px 11px 4px #898989;
   }
-
-  footer {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 40px;
-  }
-
-  footer a {
-    font-weight: bold;
-  }
-
-  @media (min-width: 480px) {
-    footer {
-      padding: 40px 0;
-    }
+  a {
+    text-decoration: none;
+    color: gray;
+    margin-right: 1rem;
   }
 </style>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,50 +1,16 @@
-<script context="module">
-  import { browser, dev } from '$app/env';
-
-  // we don't need any JS on this page, though we'll load
-  // it in dev so that we get hot module replacement...
-  export const hydrate = dev;
-
-  // ...but if the client-side router is already loaded
-  // (i.e. we came here from elsewhere in the app), use it
-  export const router = browser;
-
-  // since there's no dynamic data here, we can prerender
-  // it so that it gets served as a static asset in prod
-  export const prerender = true;
-</script>
-
-<svelte:head>
-  <title>About</title>
-  <meta name="description" content="About this app" />
-</svelte:head>
-
-<div class="content">
-  <h1>About this app</h1>
-
-  <p>
-    This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-    following into your command line and following the prompts:
-  </p>
-
-  <pre>npm init svelte</pre>
-
-  <p>
-    The page you're looking at is purely static HTML, with no client-side interactivity needed.
-    Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-    the devtools network panel and reloading.
-  </p>
-
-  <p>
-    The <a href="/todos">TODOs</a> page illustrates SvelteKit's data loading and form handling. Try using
-    it with JavaScript disabled!
-  </p>
-</div>
+<main>
+  <h1>TREASURY</h1>
+  <hr />
+  <div>The treasury is currently empty. Don't hesitate to reach out to Cosmo if you'd like to support Nomic!</div>
+</main>
 
 <style>
-  .content {
-    width: 100%;
-    max-width: var(--column-width);
-    margin: var(--column-margin-top) auto 0 auto;
+  main {
+    font-size: 1.5rem;
+    margin: 4rem;
+    padding: 2rem;
+    color: gray;
+    justify-content: center;
+    box-shadow: 4px 5px 11px 10px lightgray;
   }
 </style>
