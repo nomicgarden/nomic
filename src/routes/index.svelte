186d3767c5ae07413b7a6d61dbd3dddb7fff0a99
Cosmo <cosmo@cosmo.red>
2022-09-03 01:24:20 -0400

    Add market iframes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,7 @@
 <main>
     <h1>NOMIC</h1>
-    <a href="https://manifold.markets/cos/win-100-how-much-cash-will-nomic-ha-be4cb3906a73">[Win $100] How much cash will Nomic have in its treasury on December 31st, 2022?</a>
-    <a href="https://manifold.markets/cos/will-i-launch-my-digital-futarchy-b">Will I launch my digital futarchy before August 31st, 2022?</a>
-    <a href="https://manifold.markets/cos/how-many-unique-users-will-my-digit">How many unique users will my digital futarchy have on July 31st, 2023 if I name it "Nomic"?</a>
+    <iframe src="https://manifold.markets/embed/cos/will-i-spend-more-than-30-hours-wri" title="Will I spend more than 30 hours writing code for Nomic in September?" frameborder="0"></iframe>
+    <iframe src="https://manifold.markets/embed/cos/will-nomic-garden-feature-dynamic-e" title="Will Nomic Garden feature dynamic elements in September?" frameborder="0"></iframe>
+    <iframe src="https://manifold.markets/embed/cos/win-100-how-much-cash-will-nomic-ha-be4cb3906a73" title="[Win $100] How much cash will Nomic have in its treasury on December 31st, 2022?" frameborder="0"></iframe>
+    <iframe src="https://manifold.markets/embed/cos/how-many-unique-users-will-my-digit" title="How many unique users will my digital futarchy have on July 31st, 2023 if I name it "Nomic"?" frameborder="0"></iframe>
 </main>
