35935f3c8b36442d3df566d83fb81e36c85747c0
Cosmo <cosmo@cosmo.red>
2022-09-03 03:29:38 -0400

    Fix quotes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,5 +3,5 @@
     <iframe src="https://manifold.markets/embed/cos/will-i-spend-more-than-30-hours-wri" title="Will I spend more than 30 hours writing code for Nomic in September?" frameborder="0"></iframe>
     <iframe src="https://manifold.markets/embed/cos/will-nomic-garden-feature-dynamic-e" title="Will Nomic Garden feature dynamic elements in September?" frameborder="0"></iframe>
     <iframe src="https://manifold.markets/embed/cos/win-100-how-much-cash-will-nomic-ha-be4cb3906a73" title="[Win $100] How much cash will Nomic have in its treasury on December 31st, 2022?" frameborder="0"></iframe>
-    <iframe src="https://manifold.markets/embed/cos/how-many-unique-users-will-my-digit" title="How many unique users will my digital futarchy have on July 31st, 2023 if I name it "Nomic"?" frameborder="0"></iframe>
+    <iframe src="https://manifold.markets/embed/cos/how-many-unique-users-will-my-digit" title="How many unique users will my digital futarchy have on July 31st, 2023 if I name it &quot;Nomic&quot;?" frameborder="0"></iframe>
 </main>
