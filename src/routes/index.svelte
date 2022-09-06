d26af7511bb25c3ff957fefbe4c0c2805e0ffa31
Cosmo <cosmo@cosmo.red>
2022-09-06 00:21:12 -0400

    Drink whiskey

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -20,7 +20,7 @@ onMount(async () => {
     <iframe src="https://manifold.markets/embed/cos/will-nomic-garden-feature-dynamic-e" title="Will Nomic Garden feature dynamic elements in September?" frameborder="0"></iframe>
     <iframe src="https://manifold.markets/embed/cos/win-100-how-much-cash-will-nomic-ha-be4cb3906a73" title="[Win $100] How much cash will Nomic have in its treasury on December 31st, 2022?" frameborder="0"></iframe>
     <iframe src="https://manifold.markets/embed/cos/how-many-unique-users-will-my-digit" title="How many unique users will my digital futarchy have on July 31st, 2023 if I name it &quot;Nomic&quot;?" frameborder="0"></iframe>
-	<h1>Whiskey Drinks Menu</h1>
+	<h1>Random Questions</h1>
 	<ul>
 	{#each $drinkNames as drinkName}
 		<li>{drinkName}</li>
