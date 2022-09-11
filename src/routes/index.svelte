c7f964f48d386275fc4c1973cb7d5b0293da3c0d
Cosmo Guerini <cosmo.guerini@bostonfusion.com>
2022-09-11 19:57:33 -0400

    Fix iframe height

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,10 +16,10 @@ onMount(async () => {
 </script>
 <main>
     <h1>NOMIC</h1>
-    <iframe src="https://manifold.markets/embed/cos/will-i-spend-more-than-30-hours-wri" title="Will I spend more than 30 hours writing code for Nomic in September?" frameborder="0"></iframe>
-    <iframe src="https://manifold.markets/embed/cos/will-nomic-garden-feature-dynamic-e" title="Will Nomic Garden feature dynamic elements in September?" frameborder="0"></iframe>
-    <iframe src="https://manifold.markets/embed/cos/win-100-how-much-cash-will-nomic-ha-be4cb3906a73" title="[Win $100] How much cash will Nomic have in its treasury on December 31st, 2022?" frameborder="0"></iframe>
-    <iframe src="https://manifold.markets/embed/cos/how-many-unique-users-will-my-digit" title="How many unique users will my digital futarchy have on July 31st, 2023 if I name it &quot;Nomic&quot;?" frameborder="0"></iframe>
+    <iframe src="https://manifold.markets/embed/cos/will-i-spend-more-than-30-hours-wri" title="Will I spend more than 30 hours writing code for Nomic in September?" frameborder="0" style="height: 400px; border: none;"></iframe>
+    <iframe src="https://manifold.markets/embed/cos/will-nomic-garden-feature-dynamic-e" title="Will Nomic Garden feature dynamic elements in September?" frameborder="0" style="height: 400px; border: none;"></iframe>
+    <iframe src="https://manifold.markets/embed/cos/win-100-how-much-cash-will-nomic-ha-be4cb3906a73" title="[Win $100] How much cash will Nomic have in its treasury on December 31st, 2022?" frameborder="0" style="height: 400px; border: none;"></iframe>
+    <iframe src="https://manifold.markets/embed/cos/how-many-unique-users-will-my-digit" title="How many unique users will my digital futarchy have on July 31st, 2023 if I name it &quot;Nomic&quot;?" frameborder="0" style="height: 400px; border: none;"></iframe>
 	<h1>Random Questions</h1>
 	<ul>
 	{#each $drinkNames as drinkName}
